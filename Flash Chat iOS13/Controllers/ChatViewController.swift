79ead712cf21ad6c342ea2911dfd787b33b44a13
njdeane <njdeane7@gmail.com>
2020-12-29 12:54:53 +1000

    Create custom MessageCell Nib, register with ChatViewController

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -25,6 +25,8 @@ class ChatViewController: UIViewController {
         tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
